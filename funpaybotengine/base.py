e0cf128e929928f9ae021167671852389c48e085
qvvonk <qvvonk@gmail.com>
2025-06-29 21:39:08 +0300

    decorator

--- funpaybotengine/base.py
+++ funpaybotengine/base.py
@@ -1,21 +1,35 @@
-from __future__ import annotations
-
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from funpaybotengine.client.bot import Bot
+from funpaybotengine.client.bot import Bot
 
 
 class BindableObject:
-    def __init__(self, bot: Bot | None) -> None:
+    def __init__(self, bot: Bot | None = None) -> None:
         self._bot = bot
 
-    def as_(self, bot) -> 'BindableObject':
-        if not isinstance(self._bot, Bot):
-            raise Exception('Not a bot instance')  # todo: exceptions
-
-        self._bot = bot
+    def as_(self, bot: Bot, /) -> 'BindableObject':
+        self.bind_to(bot)
         return self
+
+    def unbind(self) -> None:
+        self._bot = None
+
+    def bind_to(self, bot: Bot, /) -> None:
+        if not isinstance(bot, Bot):
+            raise TypeError(f'{bot} is not a bot instance.')
+        self._bot = bot
 
     @property
     def bot(self) -> Bot | None:
         return self._bot
+
+
+def check_bound(func):
+    """
+    Decorator for instance methods to ensure the object is bound to any Bot instance.
+    """
+    def wrapper(self: BindableObject, *args, **kwargs):
+        if not isinstance(self, BindableObject):
+            raise Exception('Not a bindable object.')  # todo: exceptions
+        if self.bot is None:
+            raise Exception(f'{self} is not bound to any bot.')
+        return func(self, *args, **kwargs)
+    return wrapper
