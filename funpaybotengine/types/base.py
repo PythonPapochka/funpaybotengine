3354ccadabbee545ed90e3d8728850f2ad452ef7
qvvonk <qvvonk@gmail.com>
2025-07-01 17:35:53 +0300

    BaseModel config

--- funpaybotengine/types/base.py
+++ funpaybotengine/types/base.py
@@ -1,8 +1,15 @@
 __all__ = ('FunPayObject', )
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from funpaybotengine.base import BindableObject
 
 
 class FunPayObject(BindableObject, BaseModel):
+    model_config = ConfigDict(
+        frozen=True,
+        validate_assignment=True,
+        arbitrary_types_allowed=True,
+        populate_by_name=True,
+    )
+
     raw_source: str
