7fd7d960545776eb083584994e017615335e7963
qvvonk <qvvonk@gmail.com>
2025-07-01 18:38:12 +0300

    Base method

---
+++ funpaybotengine/methods/base.py
@@ -0,0 +1,34 @@
+__all__ = ('FunPayMethod', )
+
+from pydantic import BaseModel, Field
+from funpaybotengine.base import BindableObject
+from typing import Generic, TypeVar, Any, Type
+from funpayparsers.parsers.base import FunPayObjectParser, FunPayObjectParserOptions
+from http import HTTPMethod, HTTPStatus
+from abc import ABC, abstractmethod
+
+
+ReturnType = TypeVar('ReturnType', bound=Any)
+
+
+class FunPayMethod(BindableObject, BaseModel, Generic[ReturnType], ABC):
+    url: str
+    method: HTTPMethod = HTTPMethod.GET
+    headers: dict[str, str] = Field(default_factory=dict)
+    data: dict[str, str] = Field(default_factory=dict)
+    expected_status_code: int | HTTPStatus = HTTPStatus.OK
+    parser: Type[FunPayObjectParser] | None = None
+    parser_options: FunPayObjectParserOptions | None = None
+
+    def model_post_init(self, context: Any, /) -> None:
+        if self.parser and self.parser_options is None:
+            self.parser_options = self.parser.get_options_cls()()
+
+    def parse_result(self, response: str):  # todo: response is not a str, but Response obj. Implement Response obj.
+        if self.parser is None:
+            return response
+
+        return self.parser(response, options=self.parser_options).parse()
+
+    @abstractmethod
+    def transform_result(self, result: Any) -> ReturnType: ...
