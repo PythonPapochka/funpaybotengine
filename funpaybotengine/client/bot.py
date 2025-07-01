83a3138f87a06dbb6ed02f5c5f12548f1abbd3cb
qvvonk <qvvonk@gmail.com>
2025-07-01 16:53:13 +0300

    Base bot improvements

--- funpaybotengine/client/bot.py
+++ funpaybotengine/client/bot.py
@@ -1,2 +1,18 @@
+from funpaybotengine.client.session.base import BaseSession
+
+
 class Bot:
-    ...
+    def __init__(self,
+                 golden_key: str,
+                 session: BaseSession | None = None):
+        self._golden_key = golden_key
+        self._session = session or BaseSession()
+
+
+    @property
+    def golden_key(self) -> str:
+        return self._golden_key
+
+    @property
+    def session(self) -> BaseSession:
+        return self.session
