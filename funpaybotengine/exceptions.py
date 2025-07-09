2edc6ddab238bfb35f4b123752732a6304a55a04
qvvonk <qvvonk@gmail.com>
2025-07-09 10:54:00 +0300

    Session exceptions

---
+++ funpaybotengine/exceptions.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+from typing import Any, TYPE_CHECKING
+if TYPE_CHECKING:
+    from funpaybotengine.methods.base import FunPayMethod
+
+
+class FunPayRequestException(Exception):
+    def __init__(self, method: FunPayMethod[Any]) -> None:
+        self.method = method
+
+
+class UnexpectedHTTPStatus(FunPayRequestException):
+    def __init__(self, method: FunPayMethod[Any], status: int):
+        super().__init__(method=method)
+        self.status = status
+        self.expected_status = method.expected_status_code
+
+    def __str__(self):
+        return (
+            f'Unexpected response status code {self.status} for {self.method.url!r} '
+            f'(expected: {self.method.expected_status_codes!r})')
+
+
+class RateLimitExceeded(UnexpectedHTTPStatus):
+    def __init__(self, method: FunPayMethod[Any], status: int = 429):
+        super().__init__(method=method, status=status)
+
+
+class Unauthorized(UnexpectedHTTPStatus):
+    def __init__(self, method: FunPayMethod[Any], status: int = 401):
+        super().__init__(method=method, status=status)
+
+
+class Forbidden(UnexpectedHTTPStatus):
+    def __init__(self, method: FunPayMethod[Any], status: int = 403):
+        super().__init__(method=method, status=status)
+
+
+class BadRequest(UnexpectedHTTPStatus):
+    def __init__(self, method: FunPayMethod[Any], status: int = 400):
+        super().__init__(method=method, status=status)
+
+
+class NotFound(UnexpectedHTTPStatus):
+    def __init__(self, method: FunPayMethod[Any], status: int = 404):
+        super().__init__(method=method, status=status)
+
+
+class FunPayServerError(UnexpectedHTTPStatus):
+    def __init__(self, method: FunPayMethod[Any], status: int = 500):
+        super().__init__(method=method, status=status)
