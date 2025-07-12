90000a1c8122ae14642170332f7e8893a48fc297
qvvonk <qvvonk@gmail.com>
2025-07-12 20:32:53 +0300

    locale

--- funpaybotengine/client/bot.py
+++ funpaybotengine/client/bot.py
@@ -23,6 +23,7 @@ class Bot(BaseBot):
     def __init__(self, golden_key: str, session: BaseSession | None = None):
         self._golden_key = golden_key
         self._session = session or AioHttpSession(proxy=None)
+        self._locale = ''
 
     @property
     def golden_key(self) -> str:
@@ -33,7 +34,7 @@ class Bot(BaseBot):
         return self._session
 
     async def upload_chat_image(self, file: str | BytesIO) -> int:
-        return await self.make_request(UploadImage(file))
+        return await self.make_request(UploadImage(file=file))
 
     async def get_chat_history(
         self, chat_id: int | str, last_message_id: int = 999999999999
@@ -52,3 +53,7 @@ class Bot(BaseBot):
         self, method: FunPayMethod[MethodReturnType]
     ) -> MethodReturnType:
         return await self.session.make_request(method.as_(self))
+
+    @property
+    def locale(self) -> str:
+        return self._locale
