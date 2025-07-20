a747a826dc7547ab8b6a53eaf958d19bd938a276
qvvonk <qvvonk@gmail.com>
2025-07-20 11:31:49 +0300

    Filters

---
+++ funpaybotengine/dispatching/filters/base.py
@@ -0,0 +1,101 @@
+from __future__ import annotations
+
+__all__ = ('Filter', )
+
+from abc import ABC, abstractmethod
+from typing import Any, Protocol, Awaitable, Iterable, TYPE_CHECKING
+import inspect
+if TYPE_CHECKING:
+    from funpaybotengine.dispatching.events.base import Event
+
+
+class CallableFilterProtocol(Protocol):
+    def __call__(self, event: Event[Any], *args: Any, **kwargs: Any) -> bool: ...
+
+
+class AwaitableFilterProtocol(Protocol):
+    def __call__(self, event: Event[Any], *args: Any, **kwargs: Any) -> Awaitable[bool]: ...
+
+
+class Filter(ABC):
+    @abstractmethod
+    async def __call__(self, event: Event[Any], *args: Any, **kwargs: Any) -> bool: ...
+
+    def __and__(self, other: Filter) -> AndFilter:
+        return AndFilter(self, other)
+
+    def __or__(self, other: Filter) -> OrFilter:
+        return OrFilter(self, other)
+
+    def __invert__(self) -> NotFilter:
+        return NotFilter(self)
+
+
+class AndFilter(Filter):
+    def __init__(self, *filters: Filter) -> None:
+        self._filters = filters
+
+    async def __call__(self, event: Event[Any], *args: Any, **kwargs: Any) -> bool:
+        return all(
+            i(event, *args, **kwargs) for i in self._filters
+        )
+
+
+class OrFilter(Filter):
+    def __init__(self, *filters: Filter) -> None:
+        self._filters = filters
+
+    async def __call__(self, event: Event[Any], *args: Any, **kwargs: Any) -> bool:
+        return any(
+            i(event, *args, **kwargs) for i in self._filters
+        )
+
+
+class NotFilter(Filter):
+    def __init__(self, filter: Filter) -> None:
+        self._filter = filter
+
+    async def __call__(self, event: Event[Any], *args: Any, **kwargs: Any) -> bool:
+        return not self._filter(event, *args, **kwargs)
+
+
+class FilterFromFunction(Filter):
+    def __init__(self, function: CallableFilterProtocol) -> None:
+        self._function = function
+
+    async def __call__(self, event: Event[Any], *args: Any, **kwargs: Any) -> bool:
+        return self._function(event, *args, **kwargs)
+
+
+class FilterFromAsyncFunction(Filter):
+    def __init__(self, function: AwaitableFilterProtocol) -> None:
+        self._function = function
+
+    async def __call__(self, event: Event[Any], *args: Any, **kwargs: Any) -> bool:
+        return await self._function(event, *args, **kwargs)
+
+
+def  _convert_filters(
+        filters: Iterable[CallableFilterProtocol | AwaitableFilterProtocol | Filter]
+) -> list[Filter]:
+    converted_filters: list[Filter] = []
+    for i in filters:
+        if isinstance(i, Filter):
+            converted_filters.append(i)
+        elif inspect.iscoroutinefunction(i):
+            converted_filters.append(FilterFromAsyncFunction(i))
+        else:
+            converted_filters.append(FilterFromFunction(i))  # type: ignore[arg-type]
+            # checked above
+
+    return converted_filters
+
+
+def any_of(
+        filters: Iterable[CallableFilterProtocol | AwaitableFilterProtocol | Filter]
+) -> OrFilter:
+    return OrFilter(*_convert_filters(filters))
+
+
+def all_of(filters: Iterable[Filter]) -> AndFilter:
+    return AndFilter(*_convert_filters(filters))
