255e3832ab4e03b09269221bb6a73b03f09fc21d
qvvonk <qvvonk@gmail.com>
2025-07-28 12:50:24 +0300

    Runner fix

--- funpaybotengine/runner/runner.py
+++ funpaybotengine/runner/runner.py
@@ -10,16 +10,16 @@ from funpaybotengine.utils import random_runner_tag
 
 
 if TYPE_CHECKING:
-    from funpaybotengine.client.base_bot import BaseBot
+    from funpaybotengine.client.bot import Bot
 
 
-class Runner(BaseBot):
-    def __init__(self, bot: BaseBot):
+class Runner:
+    def __init__(self, bot: Bot):
         self._bot = bot
 
         self._messages_tag = random_runner_tag()
         self._counters_tag = random_runner_tag()
 
     @property
-    def bot(self) -> BaseBot:
+    def bot(self) -> Bot:
         return self._bot
