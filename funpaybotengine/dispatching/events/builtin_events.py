851bd5adb31dfe1fe28dcd3fb83034e0137120ed
qvvonk <qvvonk@gmail.com>
2025-08-02 22:16:46 +0300

    new events

--- funpaybotengine/dispatching/events/builtin_events.py
+++ funpaybotengine/dispatching/events/builtin_events.py
@@ -15,7 +15,9 @@ __all__ = (
 
 from funpaybotengine.types.chat import PrivateChatPreview
 from funpaybotengine.types.orders import OrderPreview
+from pydantic import PrivateAttr
 from funpaybotengine.types.messages import Message
+from typing import Any
 
 from .base import RunnerEvent
 
@@ -33,21 +35,58 @@ class NewMessageEvent(RunnerEvent[Message]): ...
 class CountersChangedEvent(RunnerEvent[tuple[int, int]]): ...
 
 
-class NewSaleEvent(RunnerEvent[OrderPreview]):
-    related_system_message: NewMessageEvent | None = None
-    related_auto_messages: list[NewMessageEvent] = []
+class OrderEvent(RunnerEvent[Message]):
+    _order_preview: OrderPreview | None = PrivateAttr(default=None)
+
+    def __post_init__(self, context: dict[Any, Any]) -> None:
+        self._order_preview = context.get("order_preview")
+
+    async def get_order_preview(self, update: bool = False) -> OrderPreview:
+        if self._order_preview is not None and not update:
+            return self._order_preview
+
+        assert self.bot is not None, 'Event not bound to any bot.'
+
+        return (await self.bot.get_sales(order_id_filter=self.object.meta.order_id)).orders[0]
 
 
-class SaleStatusChangedEvent(RunnerEvent[OrderPreview]):
-    previous: OrderPreview | None = None
-    related_system_message: NewMessageEvent | None = None
+class NewSaleEvent(OrderEvent): ...
 
 
-class NewPurchaseEvent(RunnerEvent[OrderPreview]):
-    related_system_message: NewMessageEvent | None = None
-    related_auto_messages: list[NewMessageEvent] = []
+class SaleStatusChangedEvent(OrderEvent): ...
 
 
-class PurchaseStatusChangedEvent(RunnerEvent[OrderPreview]):
-    previous: OrderPreview | None = None
-    related_system_message: NewMessageEvent | None = None
+class SaleClosedEvent(SaleStatusChangedEvent): ...
+
+
+class SaleClosedByAdminEvent(SaleClosedEvent): ...
+
+
+class SaleRefundedEvent(SaleStatusChangedEvent): ...
+
+
+class SalePartiallyRefundedEvent(SaleRefundedEvent): ...
+
+
+class SaleReopenedEvent(SaleStatusChangedEvent): ...
+
+
+class NewPurchaseEvent(OrderEvent): ...
+
+
+class PurchaseStatusChangedEvent(OrderEvent): ...
+
+
+class PurchaseClosedEvent(PurchaseStatusChangedEvent): ...
+
+
+class PurchaseClosedByAdminEvent(PurchaseClosedEvent): ...
+
+
+class PurchaseRefundedEvent(PurchaseStatusChangedEvent): ...
+
+
+class PurchasePartiallyRefundedEvent(PurchaseRefundedEvent): ...
+
+
+class PurchaseReopenedEvent(PurchaseStatusChangedEvent): ...
