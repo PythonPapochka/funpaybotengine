76e4853ed49e8747da6bada11b883aa4749df173
qvvonk <qvvonk@gmail.com>
2025-08-11 13:00:51 +0300

    Improved runner request

--- funpaybotengine/methods/runner_request.py
+++ funpaybotengine/methods/runner_request.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 __all__ = ('RunnerRequest',)
 
 from collections.abc import Sequence
-from typing import Literal
+from typing import Literal, TYPE_CHECKING
 
 from pydantic import BaseModel
 from funpayparsers.parsers import UpdatesParser
@@ -12,9 +12,12 @@ from funpayparsers.parsers import UpdatesParser
 from funpaybotengine.types import RunnerResponse
 from funpaybotengine.types.enums import Language
 from funpaybotengine.methods.base import FunPayMethod
-from funpaybotengine.types.requests import RunnerRequestData, RequestableObject, Action
+from funpaybotengine.types.requests import RequestableObject, Action
 from funpaybotengine.client.session.http_methods import HTTPMethod
 import json
+
+if TYPE_CHECKING:
+    from funpaybotengine.client.bot import Bot
 
 
 class RunnerRequest(FunPayMethod[RunnerResponse], BaseModel):
@@ -40,26 +43,20 @@ class RunnerRequest(FunPayMethod[RunnerResponse], BaseModel):
             method=HTTPMethod.POST,
             locale=locale,
             parser_cls=UpdatesParser,
-            data=self.serialize_as_request_data(objects_to_request, action),
+            data=make_data,
             headers={'X-Requested-With': 'XMLHttpRequest'},
 
             objects_to_request=objects_to_request,
             action=action
         )
 
-    def serialize_as_request_data(
-            self,
-            objects_to_request: Sequence[RequestableObject] | Literal[False] = False,
-            action: Action | Literal[False] = False,
-    ) -> dict[str, str | None]:
-        """Returns a dictionary suitable for runner HTTP requests."""
-        return {
-            'objects': json.dumps(
-                [i.model_dump(exclude_none=True, by_alias=True) for i in objects_to_request],
-            )
-            if objects_to_request
-            else 'false',
-            'request': action.model_dump_json(exclude_none=True, by_alias=True)
-            if action
-            else 'false',
-        }
+
+async def make_data(method: RunnerRequest, bot: Bot) -> dict[str, str]:
+    return {
+        'objects': json.dumps(
+            [await i.as_data_dict(bot) for i in method.objects_to_request]
+            if method.objects_to_request else 'false'
+        ),
+        'request': method.action.model_dump_json(exclude_none=True, by_alias=True)
+        if method.action else 'false',
+    }
