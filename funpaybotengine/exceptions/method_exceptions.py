8e25a8de541465d27428a761b34c014e5592a847
qvvonk <qvvonk@gmail.com>
2026-03-17 13:53:59 +0300

    small changes

---
+++ funpaybotengine/exceptions/method_exceptions.py
@@ -0,0 +1,20 @@
+__all__ = [
+    'MethodError',
+    'InvalidOfferFieldsError'
+]
+
+
+from .base import FunPayBotEngineError
+
+
+class MethodError(FunPayBotEngineError):
+    ...
+
+
+class InvalidOfferFieldsError(MethodError):
+    def __init__(self, message: str, fields: dict[str, str] | None = None):
+        self.message = message
+        self.fields = fields
+
+    def __str__(self):
+        return self.message
